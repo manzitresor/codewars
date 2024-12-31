31400ec316196b2ed0a70329e9168c6a4c00cea2
manzitresor <manzitre@gmail.com>
2025-01-01 00:51:45 +0200

    fix: modify balance the arrays kata

--- balance_the_arrays.ts
+++ balance_the_arrays.ts
@@ -1,13 +1,13 @@
 function balance(arr1: string[], arr2: string[]){
     const getCounts = (array:string[]) => {
-         return array.reduce((counts, item) => {
+         return array.reduce((counts, item): Record<string, number> => {
              counts[item] = (counts[item] || 0) + 1;
              return counts;
          }, {})
      }
  
-     const counts1 = Object.values(getCounts(arr1)).sort((a:string, b:string) => a - b);
-     const counts2 = Object.values(getCounts(arr2)).sort((a, b) => a - b);
+     const counts1 = Object.values(getCounts(arr1) as number).sort((a, b) => a - b);
+     const counts2 = Object.values(getCounts(arr2)as number).sort((a, b) => a - b);
  
      return JSON.stringify(counts1) === JSON.stringify(counts2);
  }
